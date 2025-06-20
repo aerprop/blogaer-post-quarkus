6700591f9c0a1c596944e80ba33877fe351dbf4e
anekra <andika.eka.102@gmail.com>
2025-06-20 14:36:06 +0700

    Improve: now injects ReactiveMongoClient and ObjectMapper instead of mongo collection and mongo database, excluding the text field from the getPostsByPage method, make all the methods work as intended.

--- src/main/java/com/blogaer/post/repositories/impl/PostRepoImpl.java
+++ src/main/java/com/blogaer/post/repositories/impl/PostRepoImpl.java
@@ -5,22 +5,24 @@ import java.util.Date;
 import java.util.List;
 import java.util.regex.Pattern;
 
-import org.bson.Document;
 import org.bson.conversions.Bson;
+import org.bson.types.ObjectId;
 
 import com.blogaer.post.dto.request.FilterDto;
 import com.blogaer.post.dto.request.PageDto;
 import com.blogaer.post.dto.request.QueryDto;
 import com.blogaer.post.entities.Post;
 import com.blogaer.post.repositories.PostRepo;
-import com.fasterxml.jackson.databind.JsonNode;
+import com.blogaer.post.utils.helper.Helper;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mongodb.client.model.Filters;
+import com.mongodb.client.model.Projections;
 import com.mongodb.client.model.Sorts;
+import com.mongodb.client.model.Updates;
 
 import io.quarkus.mongodb.FindOptions;
+import io.quarkus.mongodb.reactive.ReactiveMongoClient;
 import io.quarkus.mongodb.reactive.ReactiveMongoCollection;
-import io.quarkus.mongodb.reactive.ReactiveMongoDatabase;
 import io.smallrye.mutiny.Multi;
 import io.smallrye.mutiny.Uni;
 import jakarta.enterprise.context.ApplicationScoped;
@@ -30,10 +32,10 @@ import jakarta.inject.Inject;
 public class PostRepoImpl implements PostRepo {
 
     @Inject
-    ReactiveMongoCollection<Document> mongoCollection;
+    ReactiveMongoClient mongoClient;
 
     @Inject
-    ReactiveMongoDatabase mongoDB;
+    ObjectMapper mapper;
 
     @Override
     public Multi<Post> getPostsByPage(PageDto pageDto) {
@@ -43,6 +45,7 @@ public class PostRepoImpl implements PostRepo {
         int limit = size == 0 ? 10 : size;
         int offset = (num - 1) * limit;
         FindOptions options = new FindOptions().skip(offset).limit(limit);
+        options.projection(Projections.exclude("text"));
 
         List<Bson> filterList = new ArrayList<>();
         String userId = pageDto.getUserId();
@@ -79,64 +82,67 @@ public class PostRepoImpl implements PostRepo {
             }
             if (search != null) {
                 filterList.add(Filters.regex("title", Pattern.compile(search)));
-                filterList.add(Filters.regex("contentText", Pattern.compile(search)));
+                filterList.add(Filters.regex("text", Pattern.compile(search)));
             }
         }
-        options.filter(Filters.and(filterList));
-        ReactiveMongoCollection<Post> postCollection = mongoDB.getCollection("post", Post.class);
+        if (!filterList.isEmpty()) {
+            options.filter(Filters.and(filterList));
+        }
+        ReactiveMongoCollection<Post> postCollection = mongoClient.getDatabase("blogaer_post")
+                .getCollection("post", Post.class);
 
-        return postCollection.find(options);
+        return postCollection.find(options).map(item -> mapper.convertValue(item, Post.class));
     }
 
     @Override
     public Uni<Post> getPostById(String id) {
-        ReactiveMongoCollection<Post> postCollection = mongoDB.getCollection("post", Post.class);
-        FindOptions options = new FindOptions().filter(Filters.eq("id", id)).limit(1);
+        ReactiveMongoCollection<Post> postCollection = mongoClient.getDatabase("blogaer_post")
+                .getCollection("post", Post.class);
+        FindOptions options = new FindOptions().filter(Filters.eq("_id", new ObjectId(id))).limit(1);
 
-        return postCollection.find(options).toUni();
+        return postCollection.find(options)
+                .toUni()
+                .onItem().invoke(item -> System.out.println("post by id >> " + item));
     }
 
     @Override
     public Uni<String> addPost(Post post) {
-        ReactiveMongoCollection<Post> posCollection = mongoDB.getCollection("post", Post.class);
+        ReactiveMongoCollection<Post> postCollection = mongoClient.getDatabase("blogaer_post")
+                .getCollection("post", Post.class);
 
-        return posCollection.insertOne(post)
-                .map(result -> result.wasAcknowledged() ? "Post successfully added." : "Adding post failed!")
+        return postCollection.insertOne(post)
+                .map(result -> result.wasAcknowledged()
+                        ? result.getInsertedId().asObjectId().getValue().toHexString()
+                        : null)
                 .onFailure()
                 .recoverWithItem(error -> "Failed to add post >> " + error.getMessage());
     }
 
     @Override
     public Uni<String> patchPost(Post post) {
-        ReactiveMongoCollection<Post> posCollection = mongoDB.getCollection("post", Post.class);
-        Document setDoc = new Document();
-        ObjectMapper mapper = new ObjectMapper();
-        JsonNode node = mapper.valueToTree(post);
-        node.fields().forEachRemaining(entry -> {
-            if (!entry.getValue().isNull() && !entry.getKey().equals("_id")) {
-                setDoc.append(entry.getKey(), mapper.convertValue(entry.getValue(), Object.class));
-            }
-        });
-        if (setDoc.isEmpty()) {
-            return Uni.createFrom().failure(new IllegalArgumentException("No fields to patch in the Post object."));
-        }
-        Document patch = new Document("$set", setDoc);
-        Bson options = Filters.eq("id", post.getId());
+        ReactiveMongoCollection<Post> postCollection = mongoClient.getDatabase("blogaer_post")
+                .getCollection("post", Post.class);
 
-        return posCollection.updateOne(options, patch)
+        List<Bson> bsonList = Helper.createBsonList(mapper, post);
+        if (bsonList.isEmpty()) {
+            return Uni.createFrom().failure(new IllegalArgumentException("No fields to patch in the Post object!"));
+        }
+        Bson patch = Updates.combine(bsonList);
+        Bson options = Filters.eq("_id", new ObjectId(post.getId()));
+
+        return postCollection.updateOne(options, patch)
                 .map(result -> result.wasAcknowledged() ? "Post successfully patched." : "Patching post failed!")
-                .onFailure()
-                .recoverWithItem(error -> "Failed to patch post >> " + error.getMessage());
+                .onFailure().recoverWithItem(error -> "Failed to patch post >> " + error.getMessage());
     }
 
     @Override
     public Uni<String> deletePost(String id) {
-        ReactiveMongoCollection<Post> postCollection = mongoDB.getCollection("post", Post.class);
-        Bson filter = Filters.eq("id", id);
+        ReactiveMongoCollection<Post> postCollection = mongoClient.getDatabase("blogaer_post")
+                .getCollection("post", Post.class);
+        Bson filter = Filters.eq("_id", new ObjectId(id));
 
         return postCollection.deleteOne(filter)
                 .map(result -> result.wasAcknowledged() ? "Post successfully deleted." : "Deleting post failed!")
-                .onFailure()
-                .recoverWithItem(error -> "Failed to delete post >> " + error.getMessage());
+                .onFailure().recoverWithItem(error -> "Failed to delete post >> " + error.getMessage());
     }
 }
