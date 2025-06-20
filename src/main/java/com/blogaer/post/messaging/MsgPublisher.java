b495507b88ebbf63bb0c80aaab47e8dde0dd7136
anekra <andika.eka.102@gmail.com>
2025-06-20 14:49:28 +0700

    Improve: use byte[] instead of Object, use helper function to print out the exception.

--- src/main/java/com/blogaer/post/messaging/MsgPublisher.java
+++ src/main/java/com/blogaer/post/messaging/MsgPublisher.java
@@ -4,28 +4,49 @@ import java.util.Optional;
 
 import org.eclipse.microprofile.reactive.messaging.Channel;
 import org.eclipse.microprofile.reactive.messaging.Message;
+import org.eclipse.microprofile.reactive.messaging.Metadata;
+
+import com.blogaer.post.utils.helper.Helper;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import io.smallrye.mutiny.Uni;
 import io.smallrye.reactive.messaging.MutinyEmitter;
 import io.smallrye.reactive.messaging.rabbitmq.OutgoingRabbitMQMetadata;
+import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 
+@ApplicationScoped
 public class MsgPublisher {
 
     @Inject
-    @Channel("publisher")
-    MutinyEmitter<Object> emitter;
+    ObjectMapper mapper;
+
+    @Inject
+    @Channel("PostPubChannel")
+    MutinyEmitter<byte[]> emitter;
 
     public Uni<Void> publish(Optional<String> replyToQueue, Optional<String> correlationId, Object data) {
         if (!replyToQueue.isPresent() && !correlationId.isPresent()) {
-            return Uni.createFrom().voidItem();
+            return Helper.invokeAndLog("replyTo queue & correlationId not provided!");
         }
-        OutgoingRabbitMQMetadata.Builder metadata = OutgoingRabbitMQMetadata.builder();
-        replyToQueue.ifPresent(metadata::withRoutingKey);
-        correlationId.ifPresent(metadata::withCorrelationId);
-        metadata.build();
-        Message<Object> msg = Message.of(data).addMetadata(metadata);
+        try {
+            byte[] byteData = mapper.writeValueAsBytes(data);
+            OutgoingRabbitMQMetadata metadata = OutgoingRabbitMQMetadata.builder()
+                    .withRoutingKey(replyToQueue.get())
+                    .withCorrelationId(correlationId.get())
+                    .withContentType("application/json")
+                    .build();
+            Message<byte[]> msg = Message.of(byteData, Metadata.of(metadata));
 
-        return emitter.sendMessage(msg).invoke(item -> System.out.println("Message sent to ~> " + replyToQueue));
+            return emitter.sendMessage(msg)
+                    .onSubscription().invoke(() -> System.out.println("Publishing message..."))
+                    .onItem().invoke(item -> System.out.println("Messaged published"))
+                    .onFailure().invoke(failure -> failure.printStackTrace());
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+
+            return Helper.invokeAndLog("JsonProcessingException! >> " + e.getMessage());
+        }
     }
 }
